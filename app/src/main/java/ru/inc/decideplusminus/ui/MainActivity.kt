0cb30a790e079d2091ad5a06a5c20bf7ca6b722f
Tukin Aleksey <atukin@ozon.ru>
2021-09-30 00:52:00 +0300

    main navigation

--- app/src/main/java/ru/inc/decideplusminus/ui/MainActivity.kt
+++ app/src/main/java/ru/inc/decideplusminus/ui/MainActivity.kt
@@ -1,13 +1,9 @@
 package ru.inc.decideplusminus.ui
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import ru.inc.decideplusminus.R
 import ru.inc.decideplusminus.databinding.ActivityMainBinding
@@ -34,6 +30,7 @@ class MainActivity : AppCompatActivity() {
         navController = navHostFragment.navController
 
         binding.toolbar.setupWithNavController(navController)
+        binding.navView.setupWithNavController(navController)
 
         binding.toolbar.setNavigationOnClickListener { onBackPressed() }
     }
