ce91e24629be634bb74cff744e606c4409a35b73
Tukin Aleksey <atukin@ozon.ru>
2021-10-07 00:22:35 +0300

    начал готовить деталку простых решений

--- app/src/main/java/ru/inc/decideplusminus/ui/simple/solution/SimpleSolutionFragment.kt
+++ app/src/main/java/ru/inc/decideplusminus/ui/simple/solution/SimpleSolutionFragment.kt
@@ -2,6 +2,7 @@ package ru.inc.decideplusminus.ui.simple.solution
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import ru.inc.decideplusminus.databinding.FragmentSimpleBinding
@@ -12,11 +13,16 @@ class SimpleSolutionFragment : Fragment() {
     private var _binding: FragmentSimpleBinding? = null
     private val b get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
         FragmentSimpleBinding.inflate(inflater, container, false).also { _binding = it }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+
+    }
 }
