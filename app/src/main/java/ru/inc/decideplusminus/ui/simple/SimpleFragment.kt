96051039709569794edab5d78fea5a44ca73b269
Tukin Aleksey <atukin@ozon.ru>
2021-11-09 00:28:47 +0300

    BaseFragment

--- app/src/main/java/ru/inc/decideplusminus/ui/simple/SimpleFragment.kt
+++ app/src/main/java/ru/inc/decideplusminus/ui/simple/SimpleFragment.kt
@@ -1,42 +1,30 @@
 package ru.inc.decideplusminus.ui.simple
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import ru.inc.decideplusminus.databinding.FragmentSimpleBinding
 import ru.inc.decideplusminus.ui.MyApp
-import ru.inc.decideplusminus.ui.simple.solution.SimpleSolutionFragment
-import ru.inc.decideplusminus.ui.simple.solution.SimpleSolutionFragmentArgs
+import ru.inc.decideplusminus.ui.base.BaseFragment
 import ru.inc.decideplusminus.view_model.simple.SimpleViewModel
 import ru.inc.decideplusminus.view_model.simple.SimpleViewState
 import javax.inject.Inject
 
-class SimpleFragment : Fragment() {
-
-    //TODO базовый класс для фрагментов
+class SimpleFragment : BaseFragment<FragmentSimpleBinding>(FragmentSimpleBinding::inflate) {
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
     private val viewModel: SimpleViewModel by lazy { viewModel() }
 
     private var adapter: SimpleAdapter? = null
-    private var _binding: FragmentSimpleBinding? = null
-    private val b get() = _binding!!
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
-        FragmentSimpleBinding.inflate(inflater, container, false).also { _binding = it }.root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initRecyclerView()
         observeData()
     }
-
 
     private fun observeData() {
         MyApp.instance.appComponent.inject(this)
@@ -63,9 +51,9 @@ class SimpleFragment : Fragment() {
         }
 
         adapter = SimpleAdapter(listeners)
-        b.recycler.layoutManager = LinearLayoutManager(b.root.context, LinearLayoutManager.VERTICAL, false)
-        b.recycler.setHasFixedSize(true)
-        b.recycler.adapter = adapter
+        binding.recycler.layoutManager = LinearLayoutManager(binding.root.context, LinearLayoutManager.VERTICAL, false)
+        binding.recycler.setHasFixedSize(true)
+        binding.recycler.adapter = adapter
     }
 
     private fun renderData(stateSimple: SimpleViewState) = when (stateSimple) {
@@ -76,7 +64,6 @@ class SimpleFragment : Fragment() {
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null
         adapter = null
     }
 }
